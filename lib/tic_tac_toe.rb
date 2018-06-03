e5cbdc341472380e77c9335dd080311aaeff2453
Hayley Howell <hayley_howell@sbcglobal.net>
2018-06-03 12:08:50 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -89,14 +89,14 @@ def turn(board)
   input = gets
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index)
+    move(board, index, character)
   else 
     until valid_move?(board, index)
       puts "Please enter 1-9:"
       input = gets
       index = input_to_index(input)
     end
-    move(board, index)
+    move(board, index, character)
   end
   display_board(board)
 end
