fa749fe505235e600154d41e631eb58a1f3e9562
Hayley Howell <hayley_howell@sbcglobal.net>
2018-06-03 11:57:30 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -75,4 +75,11 @@ def move(board, index, character = "X")
   board[index] = character 
 end
 
+def valid_move?(board, index)
+  if index.between?(0,8) && position_taken?(board,index) == false
+    true
+  else
+    false
+  end
+end
 
